2a4929320766e1fc46ac262511590702c3cc8b0c
Shubham <hackshubham23@gmail.com>
2020-04-21 18:28:39 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+var app = express();
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n',(err)=>{
+        if(err){
+            console.log('Unable to append to server log');
+        }
+    });
+    next();
+});
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
+});
+app.get('/', (req,res)=>{
+    // res.send('Hello Express!');
+    res.send({
+        name:'shubham',
+        liking:['bike','car']
+    });
+});
+app.listen(3000,()=>{
+    console.log('Server is Up');
+});
